6cb0fd672f3345cf14ebc4530dbb2d2d0424d679
Anthony <thonyrivera9@gmail.com>
2024-07-16 15:51:28 -0500

    deberia de estar bien

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
 
-// Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
